f0b100cd5f8e71e9ec7aa5a48d9b358c5a391efd
sravyap975 <sravyap975@gmail.com>
2025-10-26 18:27:19 +0530

    Update digits_sum.c
    I repalced 100 with 10

--- digits_sum.c
+++ digits_sum.c
@@ -7,11 +7,12 @@ int main() {
     scanf("%d", &num);
 
     while(num != 0) {
-        digit = num % 100;
+        digit = num % 10;
         sum += digit;
-        num /= 100;
+        num /= 10;
     }
 
     printf("Sum of digits = %d\n", sum);
     return 0;
 }
+
